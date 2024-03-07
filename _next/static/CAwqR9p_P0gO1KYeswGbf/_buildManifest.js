84fe6ff28dae5a79d21c854b27f577a808fece8d
Sean Chok <seanchok@gmail.com>
2024-03-07 20:35:17 +0800

    initial commit

---
+++ _next/static/CAwqR9p_P0gO1KYeswGbf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-96f65a400c2a8b882950.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a2e830827e930b4cf92c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
